e3b0b43509d7498e532cd3d406b5ba0ff2a38449
Martin Přívozník <46354689+MartinPrivoznik@users.noreply.github.com>
2022-09-12 14:47:28 +0200

    Created project and imported UI kit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+import "../assets/css/nucleo-icons.css";
+import "../assets/scss/blk-design-system-react.scss";
+import "../assets/demo/demo.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import {Button} from 'reactstrap';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Button color="primary">Primary</Button>
+    </div>
+  )
+}
